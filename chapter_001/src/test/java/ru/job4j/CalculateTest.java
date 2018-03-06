dfbe8f1a5ca423417036866bc2827140c195f9b4
egorovasa <s.sosenkova@gmail.com>
2018-03-06 17:46:15 +0300

    1.4 JUnit[#4122]

---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,27 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+*Test
+*
+*@author Svetlana Egorova (s.sosenkova@gmail.com)
+*@version $Id$
+*@since 0.1
+*/
+public class CalculateTest {
+	/**
+	*Test echo.
+	*/ @Test
+	public void whenTakeNameThenThreeEchoPlusName() {
+	String input = "Svetlana Egorova";
+	String expect = "Echo, echo, echo : Svetlana Egorova";
+	Calculate calc = new Calculate();
+	String result = calc.echo(input);
+	assertThat(result, is(expect));
+	}
+}
