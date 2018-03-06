83404b0b133ee1fc6c21ec526faf93beb627216e
egorovasa <s.sosenkova@gmail.com>
2018-03-06 17:04:54 +0300

    1.3 JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,17 @@
+/**
+*Calculate.
+*
+*@author Svetlana Egorova (s.sosenkova@gmail.com)
+*@version $Id$
+*@since 0.1
+*/
 package ru.job4j;
 
 public class Calculate {
-	
+	/**
+	*Main.
+	*@param args - args.
+	*/
 public static void main(String[] args) {
 	System.out.println("Hello, world");
 	}
