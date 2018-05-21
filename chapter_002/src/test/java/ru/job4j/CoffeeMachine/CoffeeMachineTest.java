9f6fc625222965ddd17ba14845cc5e50d3843315
egorovasa <s.sosenkova@gmail.com>
2018-05-21 21:34:36 +0300

    8.4. Кофемашина.

---
+++ chapter_002/src/test/java/ru/job4j/CoffeeMachine/CoffeeMachineTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.CoffeeMachine;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CoffeeMachineTest {
+/**
+ * CoffeeMachineTest.
+ */
+@Test
+public void whenValueAndPriceThenLessCoinsChange() {
+        int[] expected = {10, 5};
+        int value = 50;
+        int price = 35;
+        CoffeeMachine array = new CoffeeMachine();
+        int[] result = array.changes(value, price);
+        assertThat(result, is(expected));
+}
+}
