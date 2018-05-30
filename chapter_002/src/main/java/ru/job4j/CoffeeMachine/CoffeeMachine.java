092ff3affa33e4598bfafb71f798ba5c67c54653
egorovasa <s.sosenkova@gmail.com>
2018-05-30 20:37:23 +0300

    8.4.1. Кофемашина.

--- chapter_002/src/main/java/ru/job4j/CoffeeMachine/CoffeeMachine.java
+++ chapter_002/src/main/java/ru/job4j/CoffeeMachine/CoffeeMachine.java
@@ -1,39 +1,26 @@
 package ru.job4j.CoffeeMachine;
 
-public class CoffeeMachine {
-    int[] changes(int value, int price) {
+class CoffeeMachine {
 
-        int diff = value - price;
-        int numOfTen = diff / 10;
-        int numOfFive = (diff - numOfTen * 10) / 5;
-        int numOfTwo = (diff - numOfTen * 10 - numOfFive * 5) / 2;
-        int numOfOne = (diff - numOfTen * 10 - numOfFive * 5 - numOfTwo * 2);
+    int[] changes(int value, int price) {
+        int change = value > price ? value - price : 0;
+        int[] values = {10, 5, 2, 1};
+
+        int numOfTen = change / 10;
+        int numOfFive = (change - numOfTen * 10) / 5;
+        int numOfTwo = (change - numOfTen * 10 - numOfFive * 5) / 2;
+        int numOfOne = (change - numOfTen * 10 - numOfFive * 5 - numOfTwo * 2);
         int length = numOfTen + numOfFive + numOfTwo + numOfOne;
 
-        if (length == 0) {
-            length++;
-        }
-
-        int[] result;
-        result = new int[length];
-
-        for (int i = 0; i < result.length; i++) {
-            if (numOfTen > 0) {
-                result[i] = 10;
-                numOfTen--;
-            } else if (numOfFive > 0) {
-                result[i] = 5;
-                numOfFive--;
-            } else if (numOfTwo > 0) {
-                result[i] = 2;
-                numOfTwo--;
-            } else if (numOfOne > 0) {
-                result[i] = 1;
-                numOfOne--;
-            } else {
-                result[i] = 0;
+        int[] result = new int[length];
+        int j = 0;
+            for (int i = 0; i < values.length; i++) {
+                while (change >= values[i]) {
+                    change = change - values[i];
+                    result[j] = values[i];
+                    j++;
+                }
             }
-        }
         return result;
     }
 }
