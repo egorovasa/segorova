e8fce5b1dda349f83ca450e4b5bba454d8af6a82
egorovasa <s.sosenkova@gmail.com>
2018-06-01 20:25:40 +0300

    8.5. Распарсить скобки.

---
+++ chapter_002/src/main/java/ru/job4j/Parsing/Parse.java
@@ -0,0 +1,66 @@
+package ru.job4j.Parsing;
+
+import java.util.Stack;
+/**Parse.
+ *
+ *@author Svetlana Egorova (s.sosenkova@gmail.com)
+ *
+ *@version $Id$
+ *
+ *@since 0.1
+ */
+class Parse {
+    private Stack<Character> stack = new Stack<>();
+
+    private boolean isOpeningBracket(char bracket) {
+        return "({[".indexOf(bracket) != -1;
+    }
+
+    private boolean isClosingBracket(char bracket) {
+        return ")}]".indexOf(bracket) != -1;
+    }
+
+    private boolean isPair(char opening, char closing) {
+        return opening == '(' && closing == ')' || opening == '['
+                && closing == ']' || opening == '{' && closing == '}';
+    }
+
+    boolean validate(String input) {
+        for (char c : input.toCharArray()) {
+            if (isClosingBracket(c) && stack.isEmpty()) {
+                return false;
+            }
+            if (isOpeningBracket(c)) {
+                stack.push(c);
+            }
+            if (isClosingBracket(c)) {
+                if (isPair(stack.peek(), c)) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    Stack<Character> change(String input) {
+        Stack<Character> result = new Stack<>();
+        Parse brackets = new Parse();
+        if (brackets.validate(input)) {
+            for (char c : input.toCharArray()) {
+                if (c == '[') {
+                    result.add(c);
+                    result.add(']');
+                } else if (c == '{') {
+                    result.add(c);
+                    result.add('}');
+                } else if (c == '(') {
+                    result.add(c);
+                    result.add(')');
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/Parsing/ParseTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.Parsing;
+
+import org.junit.Test;
+import java.util.Stack;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * ParseTest.
+ *
+ *@author Svetlana Egorova (s.sosenkova@gmail.com)
+ *
+ *@version $Id$
+ *
+ *@since 0.1
+ */
+public class ParseTest {
+    @Test
+    public void whenNormalBracketsThenTrue() {
+        Parse line = new Parse();
+        String test = "{[}]";
+        assertThat(line.validate(test), is(false));
+    }
+    @Test
+    public void whenDifferentOrderThenNormalOrder() {
+        String input = "[{}]";
+        Parse line = new Parse();
+        Stack<Character> expected = new Stack<>();
+        expected.add('[');
+        expected.add(']');
+        expected.add('{');
+        expected.add('}');
+        assertThat(line.change(input), is(expected));
+    }
+}
