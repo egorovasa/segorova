2901e2ef2e4da9a642b63fb56dd551b88b75db7e
egorovasa <s.sosenkova@gmail.com>
2018-07-24 13:44:20 +0300

    3.3.1. Компаратор для строк.

--- chapter_003/src/main/java/ru/job4j/sorting/ListCompare.java
+++ chapter_003/src/main/java/ru/job4j/sorting/ListCompare.java
@@ -5,49 +5,43 @@ import java.util.*;
 public class ListCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        ArrayList<Character> leftList = new ArrayList<>();
-        ArrayList<Character> rightList = new ArrayList<>();
-        for (char c : left.toCharArray()) {
-            leftList.add(c);
-        }
-        for (char c : right.toCharArray()) {
-            rightList.add(c);
-        }
-        int length1 = leftList.size();
-        int length2 = rightList.size();
+        char[] leftWorld = left.toCharArray();
+        char[] rightWorld = right.toCharArray();
 
+        int length1 = leftWorld.length;
+        int length2 = rightWorld.length;
         int rst = 0;
 
-        if (length1 == length2) {
+        if (length1 < length2) {
             for (int i = 0; i < length1; i++) {
-                rst += Character.compare(leftList.get(i), rightList.get(i));
-            }
-        } else if (length1 > length2) {
-            for (int j = 0; j < length1 - length2; j++) {
-                for (int i = 0; i < length2; i++) {
-                    if (leftList.get(i + j) != rightList.get(i)) {
-                        rst = leftList.get(i + j) - rightList.get(i);
-                        break;
-                    }
+                if (leftWorld[i] != rightWorld[i]) {
+                    rst = Character.compare(leftWorld[i], rightWorld[i]);
+                    break;
                 }
             }
             if (rst == 0) {
                 rst = length1 - length2;
             }
-        } else if (length2 > length1) {
-            for (int j = 0; j < length2 - length1; j++) {
-                for (int i = 0; i < length1; i++) {
-                    if (rightList.get(i + j) != leftList.get(i)) {
-                        rst = leftList.get(i) - rightList.get(i + j);
-                        break;
-                    }
+        } else if (length2 < length1) {
+            for (int i = 0; i < length2; i++) {
+                if (leftWorld[i] != rightWorld[i]) {
+                    rst = Character.compare(leftWorld[i], rightWorld[i]);
+                    break;
                 }
             }
             if (rst == 0) {
                 rst = length1 - length2;
+            }
+        } else if (length1 == length2) {
+            for (int i = 0; i < length1; i++) {
+                if (leftWorld[i] != rightWorld[i]) {
+                    rst = Character.compare(leftWorld[i], rightWorld[i]);
+                    break;
+                }
             }
         }
         return rst;
     }
 }
+
 
